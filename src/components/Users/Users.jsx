7e325a960dd4837f3a5c075c016e5361b3592126
Dima Yukhymuk <dima.yukhimuk@gmail.com>
2020-09-23 20:54:12 +0300

    Add pagination

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,29 +1,69 @@
-import React from "react";
+import React, {useEffect} from "react"
 import css from './Users.module.scss'
+import * as axios from 'axios'
 
 const Users = (props) => {
-  return <ul>
-    {props.users.map(
-      user => <li className={css.user} key={user.id}>
-        <div className={css.user_avatar}>
-          <img src={user.avatar} alt="User avatar"/>
-        </div>
-        {
-          user.followed
-            ? <button type='button' onClick={() => {
-              props.unfollow(user.id)
-            }}>Unfollow</button>
-            : <button type='button' onClick={() => {
-              props.follow(user.id)
-            }}>Follow</button>
-        }
-        <div className={css.user_info}>
-          <div className={css.name}>{user.name} {user.surname}</div>
-          <div className={css.status}>{user.status}</div>
-          <div className={css.address}>{user.address.country} {user.address.city}</div>
-        </div>
-      </li>
-    )}
-  </ul>
+  useEffect(
+    () => {
+      axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${props.currentPage}&count=${props.pageSize}`)
+        .then(response => {
+          props.setUsers(response.data.items)
+          props.setTotalUsersCount(response.data.totalCount)
+        })
+    }
+  )
+  const onPageChange = (p) => {
+    axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${p}&count=${props.pageSize}`)
+      .then(response => {
+        props.setUsers(response.data.items)
+      })
+    props.setCurrentPage(p)
+  }
+  const pagesCount = Math.ceil(props.totalUsersCount / props.pageSize)
+  const pages = []
+  for(let i = 1; i <= pagesCount; i++) {
+    pages.push(i)
+  }
+
+  return (
+    <div>
+      <ul className={css.pagination}>
+        {pages.map(p => {
+           return (
+             <li className={props.currentPage === p && css.active_page}
+                 onClick={() => {onPageChange(p)}}>
+               {p}
+             </li>
+           )
+          }
+        )}
+      </ul>
+      <ul>
+        {props.users.map(
+          user => <li className={css.user} key={user.id}>
+            <div className={css.user_avatar}>
+              <img src={user.photos.small !== null
+                ? user.photos.small
+                : 'https://www.iconninja.com/files/980/515/831/warrior-ninja-avatar-samurai-icon.svg'}
+                   alt="User avatar"/>
+            </div>
+            {
+              user.followed
+                ? <button type='button' onClick={() => {
+                  props.unfollow(user.id)
+                }}>Unfollow</button>
+                : <button type='button' onClick={() => {
+                  props.follow(user.id)
+                }}>Follow</button>
+            }
+            <div className={css.user_info}>
+              <div className={css.name}>{user.name}</div>
+              <div className={css.status}>{user.status}</div>
+            </div>
+          </li>
+        )}
+      </ul>
+    </div>
+  )
 }
 export default Users
--- src/components/Users/UsersContainer.jsx
+++ src/components/Users/UsersContainer.jsx
@@ -1,10 +1,19 @@
 import Users from "./Users";
 import {connect} from "react-redux";
-import {followAC, setUsersAC, unfollowAC} from "../../redux/reducers/usersReducer";
+import {
+  followAC,
+  setCurrentPageAC,
+  setTotalUsersCountAC,
+  setUsersAC,
+  unfollowAC
+} from "../../redux/reducers/usersReducer";
 
 const mapStateToProps = (state) => {
   return {
-    users: state.users.users
+    users: state.users.users,
+    totalUsersCount: state.users.totalUsersCount,
+    currentPage: state.users.currentPage,
+    pageSize: state.users.pageSize
   }
 }
 const mapDispatchToProps = (dispatch) => {
@@ -17,6 +26,12 @@ const mapDispatchToProps = (dispatch) => {
     },
     setUsers: (users) => {
       dispatch(setUsersAC(users))
+    },
+    setTotalUsersCount: (count) => {
+      dispatch(setTotalUsersCountAC(count))
+    },
+    setCurrentPage: (page) => {
+      dispatch(setCurrentPageAC(page))
     }
   }
 }
